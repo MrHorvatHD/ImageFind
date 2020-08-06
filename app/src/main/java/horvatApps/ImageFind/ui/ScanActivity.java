c54569f7704d0c94ac6a8d0c5c992c5a849c3cc7
Matej Horvat <matej@horvat.si>
2020-08-06 18:21:47 +0200

    Fix media store access bug

--- app/src/main/java/horvatApps/ImageFind/ui/ScanActivity.java
+++ app/src/main/java/horvatApps/ImageFind/ui/ScanActivity.java
@@ -15,7 +15,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
